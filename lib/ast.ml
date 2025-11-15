cde331d57276c431331dbf601b4db0309dc32f50
Finnian Brewer <finnian.m.brewer@gmail.com>
2025-11-14 19:21:49 -0800

    test: basic definition testing

--- lib/ast.ml
+++ lib/ast.ml
@@ -23,11 +23,29 @@ type expression =
   | EConcat of expression * expression
   | EFunctionCall of string * expression list
 
-type function_ = { 
-    arguments: pattern list;
-    expression: expression;
-}
+let rec expression_to_string = function
+  | EVar v -> "var:" ^ v
+  | EString s -> "string:\"" ^ s ^ "\""
+  | EConcat (first, second) ->
+      "concat(" ^ expression_to_string first ^ ", "
+      ^ expression_to_string second
+      ^ ")"
+  | EFunctionCall (name, args) ->
+      let args_str = String.concat ", " (List.map expression_to_string args) in
+      "call " ^ name ^ "(" ^ args_str ^ ")"
+
+type function_ = { arguments : pattern list; expression : expression }
 
 type definition =
   | DPattern of string * pattern
   | DFunction of string * function_
+
+let definition_to_string = function
+  | DPattern (name, pattern) ->
+      "pattern:[name=" ^ name ^ "] = " ^ pattern_to_string pattern
+  | DFunction (name, func) ->
+      let args_str =
+        String.concat ", " (List.map pattern_to_string func.arguments)
+      in
+      "function:[name=" ^ name ^ ";args=" ^ args_str ^ "] = "
+      ^ expression_to_string func.expression
--- test/test_parser.ml
+++ test/test_parser.ml
@@ -1,37 +1,63 @@
 open Matched
-open Matched.Ast
 
-let run_parsing input =
+let run_generic_parsing parse to_string input =
   match Lexer.lex input with
   | Ok tokens -> (
-      match Pattern_parser.parse_pattern tokens with
-      | Ok (pattern, _) -> print_endline @@ pattern_to_string pattern
-      | Error e -> print_endline @@ Parser_error .error_to_string e)
+      match parse tokens with
+      | Ok parsed -> print_endline @@ to_string parsed
+      | Error e -> print_endline @@ Parser_error.error_to_string e)
   | Error _ -> print_endline "Lexing error"
 
+let run_pattern_parsing input =
+  run_generic_parsing Pattern_parser.parse_pattern
+    (fun (pattern, _) -> Ast.pattern_to_string pattern)
+    input
+
+let display_defs defs =
+  String.concat "\n" @@ List.map (fun def -> Ast.definition_to_string def) defs
+
+let run_definition_parsing input =
+  run_generic_parsing Parser.parse_file display_defs input
+
 let%expect_test "simple parsing literal pattern" =
-  run_parsing {| "asdf" |};
+  run_pattern_parsing {| "asdf" |};
   [%expect {| literal:"asdf" |}]
 
 let%expect_test "simple parsing either pattern" =
-  run_parsing {| "asdf" & "other" | "should prioritize either" |};
+  run_pattern_parsing {| "asdf" & "other" | "should prioritize either" |};
   [%expect
     {| multiple[literal:"asdf"; either(literal:"other", literal:"should prioritize either")] |}]
 
 let%expect_test "simple parsing parens" =
-  run_parsing {| ("asdf" & "other") | "should prioritize either" |};
+  run_pattern_parsing {| ("asdf" & "other") | "should prioritize either" |};
   [%expect
     {| either(multiple[literal:"asdf"; literal:"other"], literal:"should prioritize either") |}]
 
 let%expect_test "simple parsing parens 2" =
-  run_parsing {| ("asdf" & "other") | ("first: " & variable & "!") |};
+  run_pattern_parsing {| ("asdf" & "other") | ("first: " & variable & "!") |};
   [%expect
     {| either(multiple[literal:"asdf"; literal:"other"], multiple[literal:"first: "; multiple[var:variable; literal:"!"]]) |}]
 
 let%expect_test "simple parsing postfix" =
-  run_parsing {| "asdf"? |};
+  run_pattern_parsing {| "asdf"? |};
   [%expect {| optional(literal:"asdf") |}]
 
 let%expect_test "simple parsing parens then optional" =
-  run_parsing {| ("asdf" & "other")? |};
+  run_pattern_parsing {| ("asdf" & "other")? |};
   [%expect {| optional(multiple[literal:"asdf"; literal:"other"]) |}]
+
+let%expect_test "parsing basic pattern" =
+  run_definition_parsing
+    {| 
+pattern awesome_pattern = ("asdf" & "other")? & "next"
+  |};
+  [%expect {| pattern:[name=awesome_pattern] = multiple[optional(multiple[literal:"asdf"; literal:"other"]); literal:"next"] |}]
+
+let%expect_test "parsing basic function definition" =
+  run_definition_parsing
+    {| 
+def main(
+	(("thing" & variable)? & "other") | "single"
+) = "hello " & name
+  |};
+  [%expect {| function:[name=main;args=either(multiple[optional(multiple[literal:"thing"; var:variable]); literal:"other"], literal:"single")] = concat(string:"hello ", var:name) |}]
